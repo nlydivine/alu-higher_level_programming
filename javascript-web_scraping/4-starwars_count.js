835a873ce356624448e1dadf8e14e8677124d5eb
nlydivine <n.lydivine@alustudent.com>
2026-02-04 10:50:17 -0800

    upgraded

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,25 @@
 const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
-  if (!error) {
-    const results = JSON.parse(body).results;
-    let count = 0;
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-    for (const movie of results) {
-      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+  const results = JSON.parse(body).results;
+  let count = 0;
+
+  for (const movie of results) {
+    // Look for character ID 18 in this movie
+    for (const charUrl of movie.characters) {
+      // Extract the ID from the URL
+      const match = charUrl.match(/\/([0-9]+)\/$/);
+      if (match && match[1] === '18') {
         count++;
+        break; // count each movie only once
       }
     }
-
-    console.log(count);
   }
+
+  console.log(count);
 });
